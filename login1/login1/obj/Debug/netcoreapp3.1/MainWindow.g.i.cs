dd8e5e580b9cc4d348fe43f830fccaac767b3e9d
sreejitha-s <sreejitha998@gmail.com>
2021-06-15 18:38:00 +0530

    label added

--- login1/login1/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ login1/login1/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace login1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/login1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/login1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
